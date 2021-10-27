3ff789cbbf6086c7479197d23f3df8ae1d241c04
rui-rui-an <951929823@qq.com>
2021-10-27 20:26:50 +0800

    提交修改

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,7 +33,7 @@ export default {
     viewPdf(content){
       const file = 'data:application/pdf;base64,' + content
       // console.log(1111111111);
-      // console.log(22222);
+      console.log(22222);
        sessionStorage.setItem("_imgUrl",file)
        window.open('/static/pdf/web/viewer.html')
     },
